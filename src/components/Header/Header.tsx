493fea1cd3bf64a0fd7453439a4087715cb9707b
Matvey Kham <mvham@mail.ru>
2022-06-17 22:06:24 +0700

    feat: project init, config enviroment

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.scss'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <h1 className='mdc-typography--headline1'>Air Recipes</h1>
+      <p className='header__subtitle'></p>
+    </div>
+  )
+}
